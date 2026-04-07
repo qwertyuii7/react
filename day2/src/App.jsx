39a81a153b633382f59fe8a4ce5f5a2289664075
qwertyuii7 <mayankchaudharyetw123@gmail.com>
2026-04-07 19:00:23 +0000

    made a todo using react

--- day2/src/App.jsx
+++ day2/src/App.jsx
@@ -3,12 +3,49 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
+  const [Todos, setTodos] = useState([{
+    title: "Go to gym",
+    description: "Hit the gym regularly",
+  }])
+
+  function Addtodo() {
+    setTodos([...Todos, {
+      title: document.getElementById("title").value,
+      description: document.getElementById("description").value
+    }])
+  }
+
+
+
 
   return (
-    <h1>Hello mayank 🚀</h1>
+    <div>
+      <input id='title' type="text" placeholder='your todo title' />
+      <input id='description' type="text" placeholder='add your description' />
+      <button onClick={Addtodo}>add todo</button>
 
+      <br />
+
+      {
+        Todos.map((todo, index) => (
+          <Todo
+            key={index}
+            title={todo.title}
+            description={todo.description}
+          />
+        ))
+      }
+    </div>
   )
+}
+function Todo(props) {
+    return (
+        <div>
+            <h3>{props.title}</h3> 
+            <p>{props.description}</p> 
+        </div>
+    );
 }
 
 export default App
