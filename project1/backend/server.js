d6e50dcfef4f8bbe90a62ebb05ce32a3b331b6ec
qwertyuii7 <mayankchaudharyetw123@gmail.com>
2026-04-28 12:37:54 +0000

    done

--- project1/backend/server.js
+++ project1/backend/server.js
@@ -14,12 +14,14 @@ connectionDB();
 app.use("/join" , joinrouter);
 app.use("/booking",bookingrouter);
 app.use("/queue",queue_router);
+app.use("/shop",shop_router);
 
 
-
+const { shop_router }=require("../backend/routes/shop_routes")
 const { queue_router } = require ("../backend/routes/queue_router")
 const { bookingrouter } = require ("../backend/routes/booking");
 const { joinrouter } = require("../backend/routes/join");
+const { shop_router } = require("./routes/shop_routes")
 
 app.get("/", (req, res) => {
   res.send("Server running");
