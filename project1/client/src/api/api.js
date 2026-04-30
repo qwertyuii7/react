b4694828cd8efe202befa5ec4314d0622213a4bc
qwertyuii7 <mayankchaudharyetw123@gmail.com>
2026-04-30 09:47:07 +0000

    some work done

--- project1/client/src/api/api.js
+++ project1/client/src/api/api.js
@@ -1,3 +1,12 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 
+export const create_shop = async(data) =>{
+    const res = await fetch(`${BASE_URL}/shop/create`,{
+        method :"POST",
+        headers :{"Content-Type":"application/json"},
+        body:JSON.stringify(data)
+    });
+    return res.json();
+}
+
