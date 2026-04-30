4843afbb7a5040b9e5226a3ed16172b043c595d9
qwertyuii7 <mayankchaudharyetw123@gmail.com>
2026-04-30 12:57:50 +0000

    some work done

--- project1/client/src/pages/shoplist.jsx
+++ project1/client/src/pages/shoplist.jsx
@@ -1,5 +1,34 @@
-import { useState } from "react";
-import { getshops } from "../api/api"; 
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getshops, getShops } from "../api/api";
+import { Link } from "react-router-dom";
+
+
+function Shoplist(){
+
+    const [shops , setShops]= useState([])
+
+    useEffect(()=>(
+
+        getshops().then(setShops)
+
+    ),[])
+
+
+    return(
+        <div>
+            <h2>All shops</h2>
+            {shops.map(shop=>(
+
+                <div key={shop._id}>                 
+                <Link to={`/shop/${shop._id}`} >{shop.name}</Link>                
+                </div>
+            ))}
+            
+        </div>
+
+    )
+}
+
+export default Shoplist ;
 
 
